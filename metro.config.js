a3dc07abc53af3c08530bb8fac05aab63c65dd82
Abubakar Shahid <152206208+abubakarshahid16@users.noreply.github.com>
2026-08-18 20:27:42 +0500

    fix: simplify metro.config.js, add 2>&1 to expo export for full output

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,6 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
+/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
-
-// Enable web support
-config.resolver.sourceExts = [...config.resolver.sourceExts, 'mjs'];
 
 module.exports = config;
