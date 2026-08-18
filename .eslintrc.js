f29ec879f8d0ef7c9f255e83bdfe247dea54cf58
Abubakar Shahid <152206208+abubakarshahid16@users.noreply.github.com>
2026-08-18 15:00:34 +0500

    Initial commit: add .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'react-native'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  settings: {
+    react: { version: 'detect' },
+  },
+  env: {
+    'react-native/react-native': true,
+    es2022: true,
+    node: true,
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'no-debugger': 'error',
+    'no-eval': 'error',
+    'no-implied-eval': 'error',
+  },
+  ignorePatterns: ['node_modules/','android/','ios/','dist/','coverage/','.expo/','babel.config.js','jest.config.js','jest.setup.ts','__mocks__/'],
+};
